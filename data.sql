bf7abce90d0a8213475af35c46f4787d00d6b67d
Leslie Aine <aineleslie@gmail.com>
2023-04-05 11:22:31 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -9,7 +9,7 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES 
 (5,'Charmander','2020-02-08',0,false,11.0),
 (6,'Plantmon','2021-11-15',2,true,5.7),
-(7,'Squirtle,'1993-04-02',3,false,12.13),
+(7,'Squirtle','1993-04-02',3,false,12.13),
 (8,'Angemon','2005-06-12',1,true,45.0),
 (9,'Boarmon','2005-06-07',7,true,20.4),
 (10,'Blossom','1998-10-13',3,true,17.0),
