9f873283b382bbebf9b4b2d43bf214952fb90e9c
LeslieAine <aineleslie@gmail.com>
2023-04-05 18:21:24 +0300

    make changes

--- schema.sql
+++ schema.sql
@@ -6,7 +6,9 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg decimal,
-    species varchar(100)
+    weight_kg decimal
 );
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+
 
