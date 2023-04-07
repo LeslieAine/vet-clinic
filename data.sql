fc946630b7a27f1984d56d0e6828a7ddf6c40c4b
LeslieAine <aineleslie@gmail.com>
2023-04-07 16:52:22 +0300

    multiple table rships

--- data.sql
+++ data.sql
@@ -57,3 +57,39 @@ UPDATE animals AS a
 SET owner_id = o.id
 FROM owners AS o
 WHERE o.full_name = (SELECT owner_name FROM animal_owner_mapping WHERE animal_name = a.name);
+
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, '2000-04-23'),
+       ('Maisy Smith', 26, '2019-01-17'),
+       ('Stephanie Mendez', 64, '1981-05-04'),
+       ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vet_id, species_id)
+VALUES (1, 1),
+       (3, 1),
+       (3, 2),
+       (4, 2);
+
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+VALUES 
+    (1, 1, '2020-05-24'),
+    (1, 3, '2020-07-22'),
+    (2, 4, '2021-02-02'),
+    (5, 2, '2020-01-05'),
+    (5, 2, '2020-03-08'),
+    (5, 2, '2020-05-14'),
+    (3, 3, '2021-05-04'),
+    (6, 4, '2021-02-24'),
+    (9, 2, '2019-12-21'),
+    (9, 1, '2020-08-10'),
+    (9, 2, '2021-04-07'),
+    (7, 3, '2019-09-29'),
+    (10, 4, '2020-10-03'),
+    (10, 4, '2020-11-04'),
+    (4, 2, '2019-01-24'),
+    (4, 2, '2019-05-15'),
+    (4, 2, '2020-02-27'),
+    (4, 2, '2020-08-03'),
+    (8, 3, '2020-05-24'),
+    (8, 1, '2021-01-11');
